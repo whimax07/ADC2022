6963c4daf298fabb300b41636a12641210264e96
adhowell <a.howell@live.co.uk>
2022-12-08 20:47:36 +0000

    Made the answer to day 8 less cringe

--- Adam/D8/C/main.c
+++ Adam/D8/C/main.c
@@ -4,56 +4,55 @@
 
 #define SIZE 255
 
-int is_visible(const int height, int* max_height, int* visible)
+int scenic_score(const int matrix[SIZE][SIZE], int row, int column, int num_rows, int num_columns, int* visible)
 {
-    if (height > *max_height || *max_height == 0) {
-        *max_height = height;
-        if (*visible == 0) {
-            *visible = 1;
-            return 1;
-        }
+    if (row == 0 || row == num_rows - 1 || column == 0 || column == num_columns - 1) {
+        *visible = 1;
+        return 0;
     }
-    return 0;
-}
 
-int scenic_score(const int matrix[SIZE][SIZE], int row, int column, int num_rows, int num_columns)
-{
     int l_score = 0;
     int r_score = 0;
     int u_score = 0;
     int d_score = 0;
+    int blocked = 0;
     int height = matrix[row][column];
     for (int r = row - 1; r >= 0; r--) {
         l_score++;
         if (matrix[r][column] >= height) {
+            blocked++;
             break;
         }
     }
     for (int r = row + 1; r < num_rows; r++) {
         r_score++;
         if (matrix[r][column] >= height) {
+            blocked++;
             break;
         }
     }
     for (int c = column - 1; c >= 0; c--) {
         u_score++;
         if (matrix[row][c] >= height) {
+            blocked++;
             break;
         }
     }
     for (int c = column + 1; c < num_rows; c++) {
         d_score++;
         if (matrix[row][c] >= height) {
+            blocked++;
             break;
         }
     }
+
+    *visible = blocked < 4;
     return l_score * r_score * u_score * d_score;
 }
 
 int main() {
 
     int matrix[SIZE][SIZE] = {0};
-    int visible[SIZE][SIZE] = {0};
 
     // Parse matrix definition
     int num_rows = 0;
@@ -80,42 +79,20 @@ int main() {
         num_rows++;
     }
 
-    // zero-effort iter over rows/columns
-    int num_trees_visible = 0;
-    int max_height;
-    for (int row = 0; row < num_rows; row++) {
-        max_height = 0;
-        for (int column = num_columns-1; column >= 0; column--) {
-            num_trees_visible += is_visible(matrix[row][column], &max_height, &visible[row][column]);
-        }
-        max_height = 0;
-        for (int column = 0; column < num_columns; column++) {
-            num_trees_visible += is_visible(matrix[row][column], &max_height, &visible[row][column]);
-        }
-    }
-    for (int column = 0; column < num_columns; column++) {
-        max_height = 0;
-        for (int row = num_rows-1; row >= 0; row--) {
-            num_trees_visible += is_visible(matrix[row][column], &max_height, &visible[row][column]);
-        }
-        max_height = 0;
-        for (int row = 0; row < num_rows; row++) {
-            num_trees_visible += is_visible(matrix[row][column], &max_height, &visible[row][column]);
-        }
-    }
-
-    printf("Total number of visible trees: %d", num_trees_visible);
-
     // Find the spot with the highest score
     int highest_score = 0;
+    int num_trees_visible = 0;
     for (int r = 0; r < num_rows; r++) {
         for (int c = 0; c < num_columns; c++) {
-            int score = scenic_score(matrix, r, c, num_rows, num_columns);
+            int v = 0;
+            int score = scenic_score(matrix, r, c, num_rows, num_columns, &v);
             if (score > highest_score) {
                 highest_score = score;
             }
+            num_trees_visible += v;
         }
     }
+    printf("\nTotal number of visible trees: %d", num_trees_visible);
     printf("\nMost scenic spot(s) score: %d", highest_score);
 
     return 0;
